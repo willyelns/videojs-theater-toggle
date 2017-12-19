767dda33592909783833baa4ace3a8a83ab298fb
Will Xavier <willyelnsg.x@gmail.com>
2017-12-19 00:06:37 -0300

    Creating a new feature: Save in LocalStorage the Theater state

--- src/plugin.js
+++ src/plugin.js
@@ -4,6 +4,11 @@ import videojs from 'video.js';
 const Plugin = videojs.getPlugin('plugin');
 const buttonName = 'vjs-theater-container-button';
 
+const config = {
+	"saveTheaterState" : true,
+	"localItemName" : 'theaterVideoState'
+}
+
 /** Class represents the TheaterToggle Plugin*/
 class TheaterToggle extends Plugin {
 
@@ -14,7 +19,41 @@ class TheaterToggle extends Plugin {
   */
   constructor(player, options) {
     super(player, options);
+
+    if (typeof(options) === "undefined") {
+  		options = config;
+  	} else {
+  		options.saveTheaterState = typeof(options.saveTheaterState) === "undefined" ? config.saveTheaterState : options.saveTheaterState;
+  		options.localItemName = typeof(options.localItemName) === "undefined" ? config.localItemName : options.localItemName;
+  	}
+  	console.log("OPTIONS: " + JSON.stringify(options));
     this.on(player, 'timeupdate', this.isTheater);
+
+    const buttonElement = document.createElement('button');
+
+    buttonElement.className = buttonName;
+
+    if(options.saveTheaterState) {
+	    if (typeof(Storage) !== "undefined") {
+	    	if (localStorage.getItem(options.localItemName) !== null) {
+	    		const storage = localStorage.getItem(options.localItemName);
+	    		if (storage === 'enabled') {
+	    			buttonElement.classList.add('theater-toggled');
+	    		}
+	    	}
+		} else {
+		    videojs.log('Sorry! No Web Storage support...');
+		}
+    } else { 
+    	if (localStorage.getItem(options.localItemName) !== null) {
+    		localStorage.removeItem(options.localItemName);
+    	}
+    }
+
+	player.on('ready', () => {
+		player.trigger('theaterMode', this.theaterMode);
+	});
+
     // Adding or removing when is fullscreen
     player.on('fullscreenchange', (event) => {
       const buttonEl = document.getElementsByClassName(buttonName)[0];
@@ -26,9 +65,6 @@ class TheaterToggle extends Plugin {
       }
     });
 
-    const buttonElement = document.createElement('button');
-
-    buttonElement.className = buttonName;
     const iconElement = document.createElement('span');
 
     iconElement.textContent = 'Theater';
@@ -43,8 +79,10 @@ class TheaterToggle extends Plugin {
     document.getElementsByClassName(buttonName)[0].onclick = function() {
       if (this.classList.contains('theater-toggled')) {
         this.classList.remove('theater-toggled');
+        localStorage.setItem(options.localItemName, 'disabled');
       } else {
         this.classList.add('theater-toggled');
+        localStorage.setItem(options.localItemName, 'enabled');
       }
       player.trigger('theaterMode', this.theaterMode);
     };
